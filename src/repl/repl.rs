2547b3896d7cb283f3e602933065bc7e517147b3
Alexander Rojas <alexander.rojas@gmail.com>
2023-02-23 19:44:44 +0100

    Create first version of the REPL

---
+++ src/repl/repl.rs
@@ -0,0 +1,23 @@
+use std::io::{self, Write};
+use token;
+use stringreader::StringReader;
+
+const PROMPT: &str = ">> ";
+
+pub fn start() -> io::Result<()> {
+  loop {
+    print!("{}", PROMPT);
+    io::stdout().flush()?;
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf)?;
+    let mut lex = lexer::Lexer::new(io::BufReader::new(StringReader::new(&buf)))?;
+    loop {
+      let tok = lex.next_token()?;
+      println!("{}", tok);
+      if tok.typ == token::TokenType::EOF {
+        break;
+      }
+    }
+    io::stdout().flush()?;
+  }
+}

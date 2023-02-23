d1332b676295de38c453f2e82be6095fc7619f19
Alexander Rojas <alexander.rojas@gmail.com>
2023-02-23 17:56:10 +0100

    Lexer scans minimal set of tokens needed for REPL

--- src/token/token.rs
+++ src/token/token.rs
@@ -12,6 +12,14 @@ pub enum TokenType {
   // Operators
   ASSIGN,
   PLUS,
+  MINUS,
+  BANG,
+  ASTERISK,
+  SLASH,
+  LT,
+  GT,
+  EQ,
+  NOT_EQ,
 
   // Delimiters
   COMMA,
@@ -24,14 +32,13 @@ pub enum TokenType {
 
   // Keywords
   FUNCTION,
-  LET
+  LET,
+  TRUE,
+  FALSE,
+  IF,
+  ELSE,
+  RETURN,
 }
-
-// impl PartialEq for TokenType {
-//   fn eq(&self, other: &Self) -> bool {
-//     self == other
-//   }
-// }
 
 impl fmt::Display for TokenType {
   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -42,6 +49,14 @@ impl fmt::Display for TokenType {
       TokenType::INT => write!(f, "INT"),
       TokenType::ASSIGN => write!(f, "="),
       TokenType::PLUS => write!(f, "+"),
+      TokenType::MINUS => write!(f, "-"),
+      TokenType::BANG => write!(f, "!"),
+      TokenType::ASTERISK => write!(f, "*"),
+      TokenType::SLASH => write!(f, "/"),
+      TokenType::LT => write!(f, "<"),
+      TokenType::GT => write!(f, ">"),
+      TokenType::EQ => write!(f, "=="),
+      TokenType::NOT_EQ => write!(f, "!="),
       TokenType::COMMA => write!(f, ","),
       TokenType::SEMICOLON => write!(f, ";"),
       TokenType::LPAREN => write!(f, "("),
@@ -50,6 +65,11 @@ impl fmt::Display for TokenType {
       TokenType::RBRACE => write!(f, "}}"),
       TokenType::FUNCTION => write!(f, "FUNCTION"),
       TokenType::LET => write!(f, "LET"),
+      TokenType::TRUE => write!(f, "TRUE"),
+      TokenType::FALSE => write!(f, "FALSE"),
+      TokenType::IF => write!(f, "IF"),
+      TokenType::ELSE => write!(f, "ELSE"),
+      TokenType::RETURN => write!(f, "RETURN"),
     }
   }
 }
@@ -69,6 +89,11 @@ impl Token {
     let typ = match s {
       "let" => TokenType::LET,
       "fn" => TokenType::FUNCTION,
+      "true" => TokenType::TRUE,
+      "false" => TokenType::FALSE,
+      "if" => TokenType::IF,
+      "else" => TokenType::ELSE,
+      "return" => TokenType::RETURN,
       _ => TokenType::IDENT
     };
     Token { typ: typ, lit: s.to_string() }
